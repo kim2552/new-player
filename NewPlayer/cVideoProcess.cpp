53ca329051b928fe6ffc050f73d593655cb29004
joohoonkim <joohoon.kim@outlook.com>
2020-12-11 00:15:21 -0500

    create delaunay triangles

--- NewPlayer/cVideoProcess.cpp
+++ NewPlayer/cVideoProcess.cpp
@@ -23,6 +23,29 @@ void dlib_point2cv_Point(dlib::full_object_detection& S, std::vector<cv::Point>&
 	}
 }
 
+static void draw_delaunay(cv::Mat& img, cv::Subdiv2D& subdiv, cv::Scalar delaunay_color)
+{
+	std::vector<cv::Vec6f> triangleList;
+	subdiv.getTriangleList(triangleList);
+	std::vector<cv::Point> pt(3);
+	cv::Size size = img.size();
+	cv::Rect rect(0, 0, size.width, size.height);
+
+	for (int i = 0; i < triangleList.size(); i++) {
+		cv::Vec6f t = triangleList[i];
+		pt[0] = cv::Point(std::round(t[0]), std::round(t[1]));
+		pt[1] = cv::Point(std::round(t[2]), std::round(t[3]));
+		pt[2] = cv::Point(std::round(t[4]), std::round(t[5]));
+
+		// Draw rectangles completely inside the image
+		if (rect.contains(pt[0]) && rect.contains(pt[1]) && rect.contains(pt[2])) {
+			cv::line(img, pt[0], pt[1], delaunay_color, 1, cv::LINE_AA, 0);
+			cv::line(img, pt[1], pt[2], delaunay_color, 1, cv::LINE_AA, 0);
+			cv::line(img, pt[2], pt[0], delaunay_color, 1, cv::LINE_AA, 0);
+		}
+	}
+}
+
 void cVideoProcess::ProcessFrame(cv::Mat& src_frame, cv::Mat& dst_frame)
 {
 	// Frontal face detector
@@ -60,16 +83,40 @@ void cVideoProcess::ProcessFrame(cv::Mat& src_frame, cv::Mat& dst_frame)
 	for (unsigned long j = 0; j < contours.size(); ++j) {
 		cv::drawContours(dst_frame, hullIndices, (int)j, (0, 255, 0), 2);
 	}
+
+	// Delaunay Triangulation
+	std::vector<cv::Point2f> points;
+	for (unsigned long j = 0; j < contours.size(); ++j) {
+		for (int i = 0; i < hullIndices[j].size(); i++) {
+			points.push_back(hullIndices[j][i]);
+		}
+	}
+
+	cv::Rect rect(0, 0, dst_frame.cols, dst_frame.rows);
+	cv::Subdiv2D subdiv(rect);
+
+	for (int i = 0; i < points.size(); i++) {
+		subdiv.insert(points[i]);
+	}
+
+	draw_delaunay(dst_frame, subdiv, (255, 255, 255));
 }
 
 void cVideoProcess::ProcessVideo()
 {
 	std::string file_path = ORIG_VIDEO_PATH;
 	std::string file_path_new = NEW_VIDEO_PATH;
+	std::string swap_file_path = SWAP_FILE_PATH;
 
 	cv::VideoCapture cap;
 	cv::Mat src_frame;
 	cv::Mat dst_frame;
+
+	// Open the image file
+	cv::Mat src_swap_img = cv::imread(swap_file_path);
+	cv::Mat dst_swap_img;
+
+	ProcessFrame(src_swap_img, dst_swap_img);
 
 	// Open the video file
 	cap.open(file_path);
--- NewPlayer/globals.h
+++ NewPlayer/globals.h
@@ -9,6 +9,7 @@
 * E-mail: joohoon.kim@outlook.com
 */
 
+#define SWAP_FILE_PATH	"C:\\Users\\jooho\\Desktop\\Sandbox\\NewPlayer\\assets\\jennifer.jpg"
 #define ORIG_VIDEO_PATH "C:\\Users\\jooho\\Desktop\\Sandbox\\NewPlayer\\assets\\joe.mp4"
 #define NEW_VIDEO_PATH	"C:\\Users\\jooho\\Desktop\\Sandbox\\NewPlayer\\assets\\joe_new.mp4"
 #define SHAPE_PREDICTOR "C:\\Users\\jooho\\Desktop\\Sandbox\\NewPlayer\\assets\\shape_predictor_68_face_landmarks.dat"
